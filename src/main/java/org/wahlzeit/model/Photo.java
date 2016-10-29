f7bb41ba9fc8dc0b92ebabb7c72691cf819a07a8
Nils Häusler <Nils Husler@DESKTOP-F14C5VP.fritz.box>
2016-10-29 12:25:17 +0200

    Created the classes. Tests excluded

--- src/main/java/org/wahlzeit/model/Photo.java
+++ src/main/java/org/wahlzeit/model/Photo.java
@@ -75,6 +75,11 @@ public class Photo extends DataObject {
 	protected String ownerId;
 	
 	/**
+	 * 
+	 */
+	public Location location;
+	
+	/**
 	 * Each photo can be viewed in different sizes (XS, S, M, L, XL)
 	 * Images are pre-computed in these sizes to optimize bandwidth when requested.
 	 */
